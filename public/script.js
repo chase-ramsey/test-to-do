2a496d1bddb7cc71297145af4a0c0fb22eb68a71
Chase Ramsey <ramsey.chase@gmail.com>
2016-05-26 14:21:24 -0500

    Complete/incomplete now updates to firebase...
    class toggling not working yet

--- public/script.js
+++ public/script.js
@@ -12,7 +12,7 @@ $(() => {
 			});
 		});
 
-	$("form").submit(() => {
+	$("form").submit((e) => {
 		$.ajax({
 			url: `${API_URL}.json`,
 			method: "POST",
@@ -31,6 +31,28 @@ $(() => {
 		});
 	});
 
+	$("tbody").on("click", ".complete", (e) => {
+		const row = $(e.target).closest("tr");
+		const id = row.data("id");
+		$.ajax({
+			url: `${API_URL}/${id}/complete.json`,
+			method: "PUT",
+			data: JSON.stringify("Complete")
+		});
+	});
+
+	$("tbody").on("click", ".undo_complete", (e) => {
+		const row = $(e.target).closest("tr");
+		const id = row.data("id");
+		$.ajax({
+			url: `${API_URL}/${id}/complete.json`,
+			method: "PUT",
+			data: JSON.stringify("Incomplete")
+		}).done(() => {
+
+		});
+	});
+
 })
 
 // CREATE: form submit event to POST data to firebase
@@ -45,15 +67,27 @@ function addItemToTable (item, id) {
 	const row = `<tr data-id="${id}">
 		<td>${item.task}</td>
 		<td>
-			<button class="btn btn-success">Complete</button>
+			<button class="btn btn-success complete">Complete</button>
+			<button class="btn btn-warning undo_complete">Undo Complete</button>
 			<button class="btn btn-danger delete">Delete</button>
 		</td>
+		<td class="status-text">${item.complete}</td>
 	</tr>`;
 	$("tbody").append(row);
+	console.log("kids: ", $(row).children(".status-text"));
+	if (item.complete === "Incomplete") {
+		console.debug("item incomplete");
+		$(row).children(".status-text").addClass("incomplete");
+	} else if (item.complete === "Complete") {
+		console.debug("item complete");
+		$(row).children(".status-text").addClass("done");
+	}
+	console.log("kids after: ", $(row).children(".status-text"));
 }
 
 function Task (description) {
 	this.task = description;
+	this.complete = "Incomplete";
 }
 
 function createItem () {
