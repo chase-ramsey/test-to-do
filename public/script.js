3d0bd6a19fa17f262db984dc8bc6d1d88bad3b06
Chase Ramsey <ramsey.chase@gmail.com>
2016-05-26 21:02:29 -0500

    Mark complete/incomplete now working as intended

--- public/script.js
+++ public/script.js
@@ -38,6 +38,10 @@ $(() => {
 			url: `${API_URL}/${id}/complete.json`,
 			method: "PUT",
 			data: JSON.stringify("Complete")
+		}).done(() => {
+			row.children(".status-text").attr("value", "Complete")
+			row.children(".status-text").text("Complete");
+			completeColor(row);
 		});
 	});
 
@@ -49,20 +53,56 @@ $(() => {
 			method: "PUT",
 			data: JSON.stringify("Incomplete")
 		}).done(() => {
-
+			row.children(".status-text").attr("value", "Incomplete");
+			row.children(".status-text").text("Incomplete");
+			completeColor(row);
 		});
 	});
 
   // Initialize Firebase
-  var config = {
-    apiKey: "AIzaSyBdk1zTU2crZvALCbJmyGqKVhmaWc_Ug34",
-    authDomain: "superproductify.firebaseapp.com",
-    databaseURL: "https://superproductify.firebaseio.com",
-    storageBucket: "superproductify.appspot.com",
-  };
-  firebase.initializeApp(config);
+//   var config = {
+//     apiKey: "AIzaSyBdk1zTU2crZvALCbJmyGqKVhmaWc_Ug34",
+//     authDomain: "superproductify.firebaseapp.com",
+//     databaseURL: "https://superproductify.firebaseio.com",
+//     storageBucket: "superproductify.appspot.com",
+//   };
+//   firebase.initializeApp(config);
+
+// const login = (email, password) => (
+//   firebase.auth().signInWithEmailAndPassword(email, password)
+// )
+
+// const register = (user, password) => (
+//   firebase.auth().createUserWithEmailAndPassword(user, password)
+// )
+
+//   $('.login form').submit((e) => {
+//     const form = $(e.target);
+//     const email = form.find('input[type="text"]').val();
+//     const password = form.find('input[type="password"]').val();
+
+//     login(email, password)
+//       .then(console.log)
+//       .catch(console.err)
+
+//     e.preventDefault()
+//   })
+
+//   $('input[value="Register"]').click((e) => {
+//     const form = $(e.target).closest('form');
+//     const email = form.find('input[type="text"]').val();
+//     const password = form.find('input[type="password"]').val();
+
+//     register(email, password)
+//       .then(() => login(email, password))
+//       .then(console.log)
+//       .catch(console.err)
+
+//     e.preventDefault()
+//   })
 
 })
+
 
 // CREATE: form submit event to POST data to firebase
 
@@ -73,25 +113,26 @@ $(() => {
 // DELETE: click event on delete to delete task
 
 function addItemToTable (item, id) {
-	const row = `<tr data-id="${id}">
+	const row = $(`<tr data-id="${id}">
 		<td>${item.task}</td>
 		<td>
 			<button class="btn btn-success complete">Complete</button>
 			<button class="btn btn-warning undo_complete">Undo Complete</button>
 			<button class="btn btn-danger delete">Delete</button>
 		</td>
-		<td class="status-text">${item.complete}</td>
-	</tr>`;
+		<td class="status-text" value="${(item.complete)}">${item.complete}</td>
+	</tr>`);
 	$("tbody").append(row);
-	console.log("kids: ", $(row).children(".status-text"));
-	if (item.complete === "Incomplete") {
-		console.debug("item incomplete");
-		$(row).children(".status-text").addClass("incomplete");
-	} else if (item.complete === "Complete") {
-		console.debug("item complete");
-		$(row).children(".status-text").addClass("done");
+	completeColor(row, item);
+}
+
+function completeColor (row) {
+	let status = row.children(".status-text").attr("value");
+	if (status === "Incomplete") {
+			row.children(".status-text").addClass("incomplete").removeClass("done");
+		} else if (status === "Complete") {
+			row.children(".status-text").addClass("done").removeClass("incomplete");
 	}
-	console.log("kids after: ", $(row).children(".status-text"));
 }
 
 function Task (description) {
